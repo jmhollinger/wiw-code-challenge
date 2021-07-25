b499f1df1e64039f58e1128c1e0ad017a99dc86c
Jonathan Hollinger <jmholl5@gmail.com>
2021-07-25 19:41:50 -0400

    Add logging and create_data_frame
    Add better logging and create_data_frame() function to take list of csv urls check for consistent structure and merge the data into a single data frame.

--- script_version/csv_processor.py
+++ script_version/csv_processor.py
@@ -1,5 +1,11 @@
 import pandas as pd
 import string
+import logging
+
+logging.basicConfig(
+level=logging.DEBUG, 
+format='%(asctime)s | %(levelname)s | %(message)s')
+
 
 def create_file_list(base_url="https://public.wiwdata.com/engineering-challenge/data/", identifier_list=list(string.ascii_lowercase), file_extension="csv"):
     """
@@ -18,3 +24,48 @@ def create_file_list(base_url="https://public.wiwdata.com/engineering-challenge/
     url_list = map(create_url, identifier_list)
 
     return list(url_list)
+
+
+def create_data_frame(url_list):
+    """
+    This function takes a list of CSV urls that have the same column layout and returns a single dataframe
+
+    url_list : A list of csv file urls with same data structure.
+
+    """
+
+    # Expected number of files
+    num_of_files = len(url_list)
+    logging.info(f"Attempting to process {num_of_files} files")
+
+    # Get column layout of first file.
+    initial_file = pd.read_csv(url_list[0])
+    column_layout = list(initial_file.columns)
+
+    # List of hold all the data frame for concatenation.
+    data_frame_list = []
+    
+    # Loop through URLS to read data
+    for url in url_list:
+        # Try to create dataframe, on error set an empty one.
+        try:
+            data_frame = pd.read_csv(url)
+        except:
+            logging.warning(f"Could not create dataframe from file at {url}")
+            data_frame = pd.DataFrame()
+            pass
+
+        if data_frame.empty != True:
+            # Check the column layout against the first file for consistency
+            data_frame_columns = list(data_frame.columns)
+            
+            if data_frame_columns == column_layout:
+                data_frame_list.append(data_frame)
+            else:
+                logging.warning(f"File {url} has an inconsistent layout.")
+
+    merged_data_frame = pd.concat(data_frame_list)
+
+    logging.info(f"Successfully processed {len(data_frame_list)} files")
+
+    return merged_data_frame
--- script_version/test_csv_processor.py
+++ script_version/test_csv_processor.py
@@ -1,12 +1,10 @@
 import pytest
 import csv_processor as csv_processor
 
-# Test Default values
+# Test create_file_list with default values
 def test_create_file_list_1():
-    print('Testing deafault values for create_file_list()')
     assert len(csv_processor.create_file_list()) == 26
     
-# Test supplied values
+# Test create_file_list with supplied values
 def test_create_file_list_2():
-    print('Testing supplied values for create_file_list()')
     assert len(csv_processor.create_file_list('https://google.com/',[1,2,3,4,5,6,7,8,9],"xls")) == 9
